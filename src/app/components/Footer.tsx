7616141f21eee57cc95b624e762cccc47ab5be22
Natasha Johnson <njohnson533@gmail.com>
2024-06-25 00:07:51 -0400

    chore: update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-primary text-white py-8  border-t border-t-secondary">
-    <div className="container mx-auto text-center">
+    <footer className="bg-primary text-white py-5  border-t border-t-secondary">
+    <div className="container mx-auto text-center flex items-center justify-center flex-wrap">
       <p>
-        &copy; {new Date().getFullYear()} <span className="text-secondary">Link Vault.</span> All rights reserved.
+        &copy; {new Date().getFullYear()} <span className="text-secondary">Link Vault. </span> All Rights Reserved.
       </p>
+      <p>Made By <Link className="text-secondary" href="https://www.natashajohnson.dev/" target="_blank">Natasha Johnson</Link></p>
     </div>
   </footer>
   )
