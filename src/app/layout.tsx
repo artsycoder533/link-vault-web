69f04b3ad2ff53e58fe70208fbeef60490bfc7e2
Natasha Johnson <njohnson533@gmail.com>
2024-06-25 00:50:42 -0400

    chore: fix og image metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  openGraph: {
+    images: '/opengraph-image.png',
+    type: 'website',
+    siteName: 'My Link Vault',
+    locale: 'en_US'
+  },
   title: "My Link Vault",
   description:
     "Link Vault is a powerful Chrome extension designed to streamline your web browsing experience and help you declutter your browser.",
@@ -17,6 +23,14 @@ export const metadata: Metadata = {
   referrer: "origin-when-cross-origin",
   category: "workflow & planning",
   verification: { google: "Oo3HrK4uDyvizqZBxy1I0cIUpNqUjm45kg0xLOtEMa4" },
+  twitter: {
+    title: "My Link Vault",
+    site: "https://my-link-vault.vercel.app/",
+    card: "summary_large_image",
+    creator: '@artsycoder533',
+    images: '/opengraph-image.png',
+    description: "Link Vault is a powerful Chrome extension designed to streamline your web browsing experience and help you declutter your browser.",
+  },
 };
 
 export default function RootLayout({
